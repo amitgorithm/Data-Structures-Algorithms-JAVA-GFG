1c0e3b765b1b379f5c27538700fea734a4f894e5
Amit Jape <japeamit@gmail.com>
2025-07-07 21:24:32 +0530

    Built-in Methods in Strings

---
+++ src/Day19Strings/BuiltInMethodsStrings.java
@@ -0,0 +1,16 @@
+package Day19Strings;
+
+public class BuiltInMethodsStrings {
+    public static void main(String[] args) {
+        String s = "Arpit Bansal";
+        System.out.println(s.indexOf('a'));
+        System.out.println(s.toUpperCase());
+
+        // startsWith() endsWith()
+        // contains()
+        // toLowerCase() toUpperCase()
+
+
+
+    }
+}
--- src/Day19Strings/stringBasics.java
+++ src/Day19Strings/stringBasics.java
@@ -1,8 +1,17 @@
 package Day19Strings;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class stringBasics {
     public static void main(String[] args) {
         // strings are character arrays
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next(); // sc.next only takes before the first space.
+        System.out.println(s);
+        String t = sc.nextLine(); //
+        System.out.println(t);
+
 
 
     }
