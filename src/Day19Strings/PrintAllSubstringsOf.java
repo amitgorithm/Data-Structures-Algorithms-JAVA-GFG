769b3af7fd243d6692451bb91a97c644aace3469
Amit Jape <japeamit@gmail.com>
2025-07-07 22:34:18 +0530

    Print all substrings of string

--- src/Day19Strings/PrintAllSubstringsOf.java
+++ src/Day19Strings/PrintAllSubstringsOf.java
@@ -4,5 +4,14 @@ public class PrintAllSubstringsOf {
     public static void main(String[] args) {
         String s = "amit";
 
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1 ; j <= n; j++) {
+                System.out.print(s.substring(i,j) + " ");
+
+            }
+            System.out.println();
+            
+        }
     }
 }
