3881cce8863499d9a0b235701652a1168505d792
Amit Jape <japeamit@gmail.com>
2025-07-14 22:02:49 +0530

    Tower of Hanoi using Recursion Pre In Post

--- src/Day24Recursions4/TowerOfHanoi.java
+++ src/Day24Recursions4/TowerOfHanoi.java
@@ -2,6 +2,19 @@ package Day24Recursions4;
 
 public class TowerOfHanoi {
     public static void main(String[] args) {
+        hanoi(5,'A','B','C');
+    }
 
+    private static void hanoi(int n, char a, char b, char c) {
+        if(n==0) return;  // base
+
+        // take n-1 discs from src to dest vis helper
+        hanoi(n-1,a,c,b);
+
+        // largest from src to dest;
+        System.out.println(a+"->"+c);
+
+        // n-1 disks from B to C via A
+        hanoi(n-1,b,a,c);
     }
 }
