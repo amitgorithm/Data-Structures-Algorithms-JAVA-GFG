c8d347eb06e01171093cd78f85350aea623a4884
Amit Jape <japeamit@gmail.com>
2025-07-22 22:33:52 +0530

    addAtTail() addAtHead() display() deleteAtHead() methods in LinkedList

--- src/LinkedList/LinkedListDataStructure.java
+++ src/LinkedList/LinkedListDataStructure.java
@@ -7,21 +7,63 @@ class Node{ // user defined data type
 
     }
 }
-class Linkedlist{ // user defined data structure
-    Node head ; // null
+class Linkedlist { // user defined data structure
+    Node head; // null
     Node tail; // null
-    void addAtTail(int val){
+    int size;
+    void addAtHead(int val){
         Node temp = new Node(val);
-        tail.next = temp;
-        tail = temp;
+        if(head==null) head = tail = temp;
+        else{
+            temp.next = head;
+            head = temp;
+        }
+        size++;
     }
-}
+    void addAtTail(int val) {
+        Node temp = new Node(val);
+        if (tail == null) head = tail = temp;
+        else {
+            tail.next = temp;
+            tail = temp;
+        }
+        size++;
 
-public class LinkedListDataStructure {
-    public static void main(String[] args) {
-        Linkedlist  ll = new Linkedlist();
-        ll.addAtTail()
+    }
+    void deleteAtHead() {
+        if(head==null){
+            System.out.println("List Is Empty");
+            return;
+        }
+        head = head.next;
+        if(head==null) tail = null;
+        size--;
+    }
+    void display(){
+        if(head==null) return;
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.val + " ");
+            temp= temp.next;
+        }
+        System.out.println();
     }
 
+    public static class LinkedListDataStructure {
+        public static void main(String[] args) {
+            Linkedlist ll = new Linkedlist();
+            System.out.println("LinkedList Size is:" + ll.size);
+            ll.addAtTail(10); ll.display();
+            ll.addAtTail(20); ll.display();
+            ll.addAtTail(30); ll.display();
+            ll.addAtTail(40); ll.display();
+            ll.addAtHead(90); ll.display();
+            ll.addAtHead(100); ll.display();
+            ll.deleteAtHead(); ll.display();
+            System.out.println("LinkedList Size is:"+ ll.size);
 
+        }
+
+
+    }
 }
