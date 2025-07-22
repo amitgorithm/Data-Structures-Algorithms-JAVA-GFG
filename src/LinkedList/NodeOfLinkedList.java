9c395a920f10ba7acb54e35da20577bf9b096264
Amit Jape <japeamit@gmail.com>
2025-07-22 20:54:24 +0530

    Nodes and Next Val

--- src/LinkedList/NodeOfLinkedList.java
+++ src/LinkedList/NodeOfLinkedList.java
@@ -6,6 +6,13 @@ class Node{
 
 public class NodeOfLinkedList {
     public static void main(String[] args) {
+        // 10 20 30 40 50
+        Node a = new Node(); a.val = 10;
+        Node b = new Node(); b.val = 20;
+        Node c = new Node(); c.val = 30;
+        Node d = new Node(); d.val = 40;
+        Node e = new Node(); e.val = 50;
+
 
     }
 }
